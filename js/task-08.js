9be53f9b270c4948d89861475feccce33b43899a
KupreichukAlex <98807339+KupreichukAlex@users.noreply.github.com>
2022-08-24 23:02:47 +0300

    finish hw-06

---
+++ js/task-08.js
@@ -0,0 +1,20 @@
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    const formEl = event.currentTarget.elements;
+
+    const email = formEl.email.value;
+    const password = formEl.password.value;
+    console.log(email);
+    console.log(password);
+    if (email === "" || password === "") {
+        alert('заполните поле ввода');
+    }
+    else {
+        console.log({ email, password });
+        form.reset();
+    }
+}
